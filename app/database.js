d60c00ccca617e2f99e350885a4ca4f4032265f1
LeoChenot <leochenot1@gmail.com>
2022-08-18 13:36:22 +0200

    heroku

--- app/database.js
+++ app/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const client = new Sequelize(process.env.PG_URL, {
+const client = new Sequelize(process.env.DATABASE_URL, {
   define: {
     updatedAt: 'updated_at',
     createdAt: 'created_at',
